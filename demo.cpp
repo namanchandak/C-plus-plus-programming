3d86f91054107f1a77a1e7dd6bcde99c797c0b15
namanchandak <namanchandak@namans-MacBook-Pro-2.local>
2025-11-03 18:47:37 +0530

    cp

--- demo.cpp
+++ demo.cpp
@@ -1,12 +1,72 @@
 #include <iostream>
+#include <set>
+#include <vector>
+#include <map>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+#define ll long long int
+#include <unordered_map>
 using namespace std;
 
-int main() {
+void sol()
+{
+    ll n, ans = -1;
+    cin >> n;
+    priority_queue<ll> pq;
+    for(int i=0;i<n;i++)
+    {
+        ll x;
+        cin >> x;
+        pq.push(-1*x);
+    }
 
-    int a;
-    cin>>a;
-  
-    cout << "Hello World " << a;
+    int node = pq.top() * -1;
+    pq.pop();
 
+    if(node>0)
+    {
+        ans = 0;
+        cout << ans << endl;
+        return;
+    }
+    
+    while(pq.size())
+    {
+        int x = -1*pq.top();
+
+        if(node +1 < x)
+        {
+            // cout<< node <<"yes"<<endl;
+            ans = node + 1;
+            break;
+        }
+        node = x;
+        pq.pop();
+
+    }
+
+    if(ans == -1)
+    {
+        ans = node + 1;
+    }
+    cout << ans << endl;
+
+    
+}
+
+//////////////////////=======================///////////////////////////////
+
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        sol();
+    
+    }
     return 0;
 }
+
+
