90629647a7e8372ec072dab2d5d7566eb57e515c
Naman <naman@Apples-MacBook-Pro.local>
2025-12-17 22:14:12 +0530

    task json

--- demo.cpp
+++ demo.cpp
@@ -5,14 +5,22 @@
 #include <algorithm>
 #include <queue>
 #include <cmath>
+#include<bits/std.c++>
 #define ll long long int
 #define ve vector<ll>
+
 #include <unordered_map>
 using namespace std;
 
 void sol()
 {
+    int n;
+    cin>> n;
 
+    vector<int>v;
+    v.push_back(3)
+
+    cout<< n << "naman" << v.back(); 
 }
 
 //////////////////////=======================///////////////////////////////
